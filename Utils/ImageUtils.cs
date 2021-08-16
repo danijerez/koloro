223762f8b624d34c9efea8967b9cffeab9c3b256
dani <danieljerezgarrido@gmail.com>
2021-08-16 02:21:18 +0200

    refactor

---
+++ Utils/ImageUtils.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace koloro.Utils
+{
+    public class ImageUtils
+    {
+        public static async Task FilterImage(IJSRuntime js, string name)
+        {
+            await js.InvokeAsync<object>("saveAsFile", name + ".png");
+        }
+    }
+}
