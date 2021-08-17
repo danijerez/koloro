4d1a1d638ec0727a7d04d35cd3b5e247fd7110a4
dani <danieljerezgarrido@gmail.com>
2021-08-17 12:47:04 +0200

    init pokemon

---
+++ Core/Models/PokemonData.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json.Serialization;
+
+namespace koloro.Core.Models
+{
+    public class DreamWorld
+    {
+        public string front_default { get; set; }
+        public object front_female { get; set; }
+    }
+
+    public class OfficialArtwork
+    {
+        public string front_default { get; set; }
+    }
+
+    public class Other
+    {
+        public DreamWorld dream_world { get; set; }
+
+        [JsonPropertyName("official-artwork")]
+        public OfficialArtwork OfficialArtwork { get; set; }
+    }
+
+
+    public class Sprites
+    {
+        public Other other { get; set; }
+    }
+
+
+    public class PokemonData
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int order { get; set; }
+        public Sprites sprites { get; set; }
+    }
+}
---
+++ Core/RestClients/PokeApiRestClient.cs
@@ -0,0 +1,45 @@
+﻿using koloro.Core.Models;
+using Microsoft.AspNetCore.WebUtilities;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace koloro.Core.RestClients
+{
+    public static class PokeApiRestClient
+    {
+        private static readonly string host = "https://pokeapi.co/api/v2";
+
+
+        public static async Task<PokemonResponse> Pokemon(HttpClient http, int offset, int limit)
+        {
+            http.DefaultRequestHeaders.Add("Accept", "application/json");
+            var query = new Dictionary<string, string>
+            {
+                ["offset"] = offset.ToString(),
+                ["limit"] = limit.ToString(),
+            };
+
+            var httpResponse = await http.GetAsync(QueryHelpers.AddQueryString(string.Format("{0}/pokemon", host), query));
+            var response = await httpResponse.Content.ReadFromJsonAsync<PokemonResponse>();
+            return response;
+        }
+
+        public static async Task<PokemonData> PokemonDataByName(HttpClient http, string name)
+        {
+            var httpResponse = await http.GetAsync(string.Format("{0}/pokemon/{1}", host, name));
+            var response = await httpResponse.Content.ReadFromJsonAsync<PokemonData>();
+
+            if (response != null)
+            {
+                var bytes = await http.GetByteArrayAsync(response.sprites.other.OfficialArtwork.front_default);
+                response.sprites.other.OfficialArtwork.front_default = "data:image/png;base64," + Convert.ToBase64String(bytes);
+            }
+
+            return response;
+        }
+
+    }
+}
