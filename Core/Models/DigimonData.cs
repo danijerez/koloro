d57b66639d4520d4dfab34f2fd72e3f4fde95ae1
dani <danieljerezgarrido@gmail.com>
2021-08-19 00:51:28 +0200

    add digimon

---
+++ Core/Models/DigimonData.cs
@@ -0,0 +1,9 @@
+﻿namespace koloro.Core.Models
+{
+    public class DigimonData
+    {
+        public string name { get; set; }
+        public string img { get; set; }
+        public string level { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -16,7 +16,6 @@ namespace koloro
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddMudServices();
-
             await builder.Build().RunAsync();
         }
     }
