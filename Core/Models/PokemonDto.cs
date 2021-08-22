2b6b801b21c018cb230d040b9241217585128c4b
dani <danieljerezgarrido@gmail.com>
2021-08-22 13:08:22 +0200

    add refactor json pokemon

---
+++ Core/Models/PokemonDto.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace koloro.Core.Models
+{
+    public class PokemonDto
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int height { get; set; }
+        public int weight { get; set; }
+        public Art art { get; set; }
+        public List<TypePokemon> types { get; set; }
+    }
+
+    public class Art
+    {
+        public string official { get; set; }
+        public string dreamworld { get; set; }
+        public SpritesPokemon sprites { get; set; }
+    }
+
+    public class SpritesPokemon
+    {
+        public string back_default { get; set; }
+        public string back_female { get; set; }
+        public string back_shiny { get; set; }
+        public string back_shiny_female { get; set; }
+        public string front_default { get; set; }
+        public string front_female { get; set; }
+        public string front_shiny { get; set; }
+        public string front_shiny_female { get; set; }
+    }
+
+    public class TypePokemon
+    {
+        public int slot { get; set; }
+        public string name { get; set; }
+    }
+
+    public class AbilityPokemon
+    {
+        public string name { get; set; }
+        public int slot { get; set; }
+        public bool hidden { get; set; }
+    }
+}
