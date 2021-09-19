b86d5eeb71852a106732d2898b49c148c42b7566
dani <danieljerezgarrido@gmail.com>
2021-09-19 03:08:51 +0200

    add languages

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
+using AKSoftware.Localization.MultiLanguages;
+using Blazored.LocalStorage;
 using Blazored.Toast;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using MudBlazor.Services;
 using System;
 using System.Net.Http;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace koloro
@@ -14,10 +17,11 @@ namespace koloro
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
-
+            builder.Services.AddLanguageContainer(Assembly.GetExecutingAssembly());
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddMudServices();
             builder.Services.AddBlazoredToast();
+            builder.Services.AddBlazoredLocalStorage();
             await builder.Build().RunAsync();
         }
     }
